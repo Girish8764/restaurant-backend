7c9d5b6fb99517e02b81d241113c1edc4e5bd9bf
root <root@ip-172-31-6-186.ap-south-1.compute.internal>
2026-06-09 14:19:16 +0000

    change output to check code

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use("/api/foods", foodRoutes);
 
 app.get("/health", (req, res) => {
   res.status(200).json({
-    status: "UP",
+    status: "WORKING",
     service: "restaurant-backend"
   });
 });
